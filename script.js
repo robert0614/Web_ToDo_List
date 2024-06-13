ddfa7c88e0b17566e54b34cc532f31e0078b5e57
robert0614 <robert.mella@stmartin.com>
2024-06-13 16:26:06 -0700

    First Commit

--- script.js
+++ script.js
@@ -1,67 +1,33 @@
-let randomNumber = Math.floor(Math.random() * 100) + 1;
-const guesses = document.querySelector(".guesses");
-const lastResult = document.querySelector(".lastResult");
-const lowOrHi = document.querySelector(".lowOrHi");
-const guessSubmit = document.querySelector(".guessSubmit");
-const guessField = document.querySelector(".guessField");
-let guessCount = 1;
-let resetButton;
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const todoForm = document.getElementById('todo-form');
+    const todoList = document.getElementById('todo-list');
 
-function checkGuess() {
-  const userGuess = Number(guessField.value);
-  if (guessCount === 1) {
-    guesses.textContent = "Previous guesses: ";
-  }
+    todoForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const text = document.getElementById("todo-input").value;
+        console.log('text', text);
+        addTodo(text)
+    });
 
-  guesses.textContent += userGuess + " ";
+    /**
+     * 
+     * @param {string} str // add a new string value to the list
+     */
+    function addTodo(str) {
+    const li = document.createElement("li");
+    li.textContent = str;
+    li.className = "todo-item"
 
-  if (userGuess === randomNumber) {
-    lastResult.textContent = "Congratulations! You got it right!";
-    lastResult.style.backgroundColor = "green";
-    lowOrHi.textContent = "";
-    setGameOver();
-  } else if (guessCount === 10) {
-    lastResult.textContent = "!!!GAME OVER!!!";
-    lowOrHi.textContent = "";
-    setGameOver();
-  } else {
-    lastResult.textContent = "Wrong!";
-    lastResult.style.backgroundColor = "red";
-    if (userGuess < randomNumber) {
-      lowOrHi.textContent = "Last guess was too low!";
-    } else if (userGuess > randomNumber) {
-      lowOrHi.textContent = "Last guess was too high!";
-    }
-  }
+    const removeButton = document.createElement("button");
+    removeButton.textContent = 'Remove';
+    removeButton.className = "removeButton";
 
-  guessCount++;
-  guessField.value = "";
-  guessField.focus();
+    removeButton.addEventListener('click', () => {
+        todoList.removeChild(li);
+    })
+
+    li.appendChild(removeButton);
+    todoList.appendChild(li);
 }
-
-guessSubmit.addEventListener("click", checkGuess);
-
-function setGameOver() {
-  guessField.disabled = true;
-  guessSubmit.disabled = true;
-  resetButton = document.createElement("button");
-  resetButton.textContent = "Start new game";
-  document.body.appendChild(resetButton);
-  resetButton.addEventListener("click", resetGame);
-}
-
-function resetGame() {
-  guessCount = 1;
-  const resetParas = document.querySelectorAll(".resultParas p");
-  for (const resetPara of resetParas) {
-    resetPara.textContent = "";
-  }
-
-  resetButton.parentNode.removeChild(resetButton);
-  guessField.disabled = false;
-  guessSubmit.disabled = false;
-  guessField.value = "";
-  guessField.focus();
-  lastResult.style.backgroundColor = "white";
-  randomNumber = Math.floor(Math.random() * 100) + 1;
-}
+});
